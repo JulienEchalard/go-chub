08b7d809770d7a51457dcf1199e6ced48021b2c9
JulienEchalard <julien.echalard@gmail.com>
2026-09-14 19:59:18 +0200

    Update href for first item in items array

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,7 @@
     {
       title: "Reaching caesarean section rates below 15%: experience of a French tertiary maternity using the Robson classification system",
       subtitle: "Obstétrique · Echalard et al. · Reproductive Health 2025 · PDF",
-      href: "protocoles/obstetrique/reaching-caesarean-section-rates-below-15-percent-robson.pdf",
+      href: "protocoles/obstétrique/Publication.pdf",,
       keywords: "cesarean caesarean césarienne cesarienne robson classification accouchement voie basse taux césarienne echalard publication thèse these obstétrique"
     },
     { title: "Protocoles d’obstétrique", subtitle: "Documents PDF obstétricaux", href: "protocoles/obstetrique/index.html", keywords: "protocole obstétrique obstetrique pdf" },
